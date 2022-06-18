de08531895d1ddeb62ad4bb58b42bf2a927eefc3
Zhaohan Sun <szh6615nku@gmail.com>
2022-06-18 15:02:02 -0400

    update conftest.py

--- test_pipeline/conftest.py
+++ test_pipeline/conftest.py
@@ -2,7 +2,6 @@ import pytest
 import os
 import numpy as np
 import pandas as pd
-from ml_pipeline.ml.data import load_data
 from fastapi.testclient import TestClient
 
 
@@ -14,7 +13,8 @@ def data():
     root_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
 
     file_name = "census.csv"
-    file_path = os.path.join(root_path, "data")
+    file_path = os.path.join(root_path, "data", file_name)
 
-    return load_data(file_path, file_name)
+    df = pd.read_csv(file_path, low_memory=False)
+    return df
 
